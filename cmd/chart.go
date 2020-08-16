04e6f43e9fb368a4a80e3cdac20144583116b4fa
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-08-16 21:08:15 +0430

    ssh

--- cmd/chart.go
+++ cmd/chart.go
@@ -8,7 +8,8 @@ import (
 var chartCmd = &cobra.Command{
 	Use:   "chart",
 	Short: "Charting tool",
-	Long: `Using chart command it is possible to create different kind of charts`,
+	Long: `Using chart command it is possible to create different kind of charts.
+Use one of the available charts below to create now!`,
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
